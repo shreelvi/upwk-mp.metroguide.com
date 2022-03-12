2ae4972eb281ae7c5fbe993564b67194dbccfcd1
Elvis Shrestha <elvisshrestha@Elviss-MBP.attlocal.net>
2022-03-12 14:07:51 -0800

    Added weboptimizer package for bundling. Migrated bundling

--- mp.metroeguide.web/Startup.cs
+++ mp.metroeguide.web/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +19,61 @@ namespace mp.metroeguide.web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddWebOptimizer(pipeline => 
+            {
+                //CSS
+                pipeline.AddCssBundle("/css/bundle.css", 
+                    "css/site.css", "css/bootstrap.css", "css/toastr.min.css");
+
+                //JS
+                pipeline.AddJavaScriptBundle("/js/bundles/jquery.js", 
+                    "js/scripts/vendor/jquery-1.10.2.js");
+
+                pipeline.AddJavaScriptBundle("/js/bundles/bootstrap.js", 
+                    "js/scripts/vendor/bootstrap.js", 
+                    "js/scripts/vendor/respond.js");
+
+                pipeline.AddJavaScriptBundle("/js/bundles/knockout.js", 
+                    "js/scripts/vendor/knockout-3.3.0.js", 
+                    "js/scripts/koHandlers/showModal.js", 
+                    "js/scripts/vendor/pager.min.js", 
+                    "js/scripts/vendor/moment.js", 
+                    "js/scripts/vendor/toastr.min.js");
+
+                pipeline.AddJavaScriptBundle("/js/bundles/site.js", 
+                    "js/scripts/dataService.js", 
+                    "js/scripts/reusables/foldersList.js");
+
+                pipeline.AddJavaScriptBundle("/js/bundles/clientManager.js", 
+                    "js/scripts/clientManager/models.js",
+                    "js/scripts/clientManager/clientVm.js");
+
+                pipeline.AddJavaScriptBundle("/js/bundles/pageCopier.js", 
+                    "js/scripts/copy/models.js", 
+                    "js/scripts/copy/copyVm.js");
+
+                pipeline.AddJavaScriptBundle("/js/bundles/manage.js", 
+                    "js/scripts/reusables/foldersList.js",
+                    "js/scripts/manage/models.js", 
+                    "js/scripts/manage/manageVm.js");
+                
+                pipeline.AddJavaScriptBundle("/js/bundles/pageManage.js", 
+                    "js/scripts/pageManage/models.js",
+                    "js/scripts/pageManage/pageManageVm.js");
+
+                pipeline.AddJavaScriptBundle("js/bundles/linkViewer.js", 
+                    "js/scripts/koHandlers/iFrameBinder.js",
+                    "js/scripts/models/LinkModel.js", 
+                    "js/scripts/linkViewer/linkViewerVm.js");
+
+                pipeline.AddJavaScriptBundle("js/bundles/findReplace.js", 
+                    "js/scripts/findReplace/findReplaceVm.js");
+
+                pipeline.AddJavaScriptBundle("js/bundles/linkChecker.js", 
+                    "js/scripts/koHandlers/img.js", 
+                    "js/scripts/linkChecker/models.js",
+                    "js/scripts/linkChecker/linkCheckerVm.js");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +90,7 @@ namespace mp.metroeguide.web
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+            app.UseWebOptimizer();
             app.UseStaticFiles();
 
             app.UseRouting();
